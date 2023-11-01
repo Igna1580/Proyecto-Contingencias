be4fc098977d04f22c3473efc14aac4c33594e37
Igna1580 <jose.rojaszarate@ucr.ac.cr>
2023-11-01 12:06:27 -0600

    Creacion
    Subir primeros archivos

---
+++ Tabla de vida.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(janitor)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(readxl)
+
+
+#--- Abrir base de datos -------------------------------------------------------
+# Supuesto de distribucion uniforme de muertes
+# Supuesto de edad maxima es 110 años
+# 
+
+
+
+Hombres_2023_demografia <- read_excel("repoblacev2011-2050-05_2.xlsx", sheet = "Cuadro 5 ", range = "G1017:G1177")
+Mujeres_2023_demografia <- read_excel("repoblacev2011-2050-05_2.xlsx", sheet = "Cuadro 5 ", range = "G1857:G2017")
+
+colnames(Hombres_2023_demografia) <- c("lx")
+colnames(Mujeres_2023_demografia) <- c("lx")
+
+
+filtro <- c()
+for (i in 0:19){
+  filtro <- c(filtro, (4+i*8):(8+i*8))
+}
+
+Hombres_2023_demografia <- Hombres_2023_demografia[filtro,1]
+Mujeres_2023_demografia <- Mujeres_2023_demografia[filtro,1]
+
+Prob_Trans_Hombres <- read.csv("ProbTransHombres.csv", sep = ";")
+Prob_Trans_Mujeres <- read.csv("ProbTransMujeres.csv", sep = ";")
+
+
+
+
