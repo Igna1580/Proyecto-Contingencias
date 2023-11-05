ee5b1ed63ecf26cab741e138fac5c66c398c92cb
Igna1580 <jose.rojaszarate@ucr.ac.cr>
2023-11-05 16:28:53 -0600

    Legibilidad
    Mejora de legibilidad, uso de indices, y cambio de nombres

--- Tabla de vida.R
+++ Tabla de vida.R
@@ -6,16 +6,16 @@ library(plotly)
 library(readxl)
 library("papaja")
 
-#--- Abrir base de datos -------------------------------------------------------
+#--- Abrir Base de Datos -------------------------------------------------------
 # Supuesto de distribucion uniforme de muertes
 # Supuesto de edad maxima es 110 años
-# inflación, promedio de los últimos 20 años
+# inflación, promedio de los últimos 10 años
 
 Hombres_2023_demografia <- read_excel("repoblacev2011-2050-05_2.xlsx", sheet = "Cuadro 5 ", range = "G1017:G1177")
 Mujeres_2023_demografia <- read_excel("repoblacev2011-2050-05_2.xlsx", sheet = "Cuadro 5 ", range = "G1857:G2017")
 
-colnames(Hombres_2023_demografia) <- c("poblacion_Hombres")
-colnames(Mujeres_2023_demografia) <- c("poblacion_Mujeres")
+colnames(Hombres_2023_demografia) <- c("pob_H")
+colnames(Mujeres_2023_demografia) <- c("pob_M")
 
 
 filtro <- c()
@@ -41,26 +41,26 @@ porcentajes <- c(0.05, 0.05, 0.8, 0.08, 0.10, 0.10, 0.15, 0.15, 0.20, 0.20, 0.25
 # Crear un dataframe para las edades
 edades_df <- data.frame(Edad = edades-1)
 
-edades_seleccionadas_hombres <- Hombres_2023_demografia[edades,] 
+edades_selec_H <- Hombres_2023_demografia[edades,] 
 
 # Unir el dataframe de edades con el dataframe de hombres
-edades_seleccionadas_hombres <- cbind(edades_df, edades_seleccionadas_hombres)
+edades_selec_H <- cbind(edades_df, edades_selec_H)
 
 
-# Luego puedes agregar este vector de porcentajes al dataframe edades_seleccionadas_hombres
-edades_seleccionadas_hombres$Porcentajes_Diferentes <- porcentajes
+# Luego puedes agregar este vector de porcentajes al dataframe edades_selec_H
+edades_selec_H$porc_estimados <- porcentajes
 
 # Calcular la población por edad (multiplicar la cantidad de hombres por el porcentaje)
-edades_seleccionadas_hombres$Poblacion_por_Edad <- edades_seleccionadas_hombres$poblacion_Hombres* edades_seleccionadas_hombres$Porcentajes_Diferentes
+edades_selec_H$pob_estimada<- edades_selec_H$pob_H* edades_selec_H$porc_estimados
 
 # Caso Mujeres 
-edades_seleccionadas_mujeres <- Mujeres_2023_demografia[edades,] 
-edades_seleccionadas_mujeres <- cbind(edades_df, edades_seleccionadas_mujeres)
-edades_seleccionadas_mujeres$Porcentajes_Diferentes <- porcentajes
-edades_seleccionadas_mujeres$Poblacion_por_Edad <- edades_seleccionadas_mujeres$poblacion_Mujeres * edades_seleccionadas_mujeres$Porcentajes_Diferentes
+edades_selec_M <- Mujeres_2023_demografia[edades,] 
+edades_selec_M <- cbind(edades_df, edades_selec_M)
+edades_selec_M$porc_estimados <- porcentajes
+edades_selec_M$pob_estimada<- edades_selec_M$pob_M * edades_selec_M$porc_estimados
 
 
-#--- Obtencion de inflacio e interes -------------------------------------------
+#--- Inflacio e Interes --------------------------------------------------------
 
 # inflación en Costa Rica de los últimos 10 años según Base de datos del Fondo Monetario Internacional, Banco Mundial e indicador del IPC de la OCDE
 inflacion_data <- data.frame(
@@ -80,7 +80,7 @@ descuento <- mean(tasas_Descuento$`3 meses`)
 
 
 
-#--- Obtencion de Probabilidades -----------------------------------------------
+#--- Probabilidades ------------------------------------------------------------
 
 ##--- Creacion de Dataframe con datos actuariales ------------------------------
 #Esta funcion crea un dataframe con la pyoyeccion de personas en 
@@ -122,7 +122,7 @@ obtencion_tabla_proyeccion <- function(x,status,sexo) {
   return (tabla)
 }
 
-##--- Funcion de obtencion de probabilidades------------------------------------
+##--- Funcion de Obtencion de Probabilidades------------------------------------
 
 tPx_ij <- function(t=1,x=65,i=0,j=0,sexo){
   p <- obtencion_tabla_proyeccion(x,i,sexo)[t+1,j+2]
@@ -138,77 +138,73 @@ ax.n_ij <- function(x,n,i=0,j,r=5.8,inf=2.8818,sexo){
   return(resultado)
 }
 
-#--- Prima inclusiva -----------------------------------------------
+#--- Calculo Prima Generalizada -----------------------------------------------
 
-A <- 183671.1703
-B <- 363381.6771
-C <- 740398.8263
-D <- 1572738.998
+A <- 183671.1703 #Beneficio estado 1
+B <- 363381.6771 #Beneficio estado 2
+C <- 740398.8263 #Beneficio estado 3
+D <- 1572738.998 #Beneficio estado 4
 
+##--- Beneficios ---------------------------------------------------------------
 # Crear un vector vacío para almacenar los resultados de hombres
-
-beneficios_hombres <- data.frame(Beneficios_hombres = numeric())
-
+benef_H_indiv <- data.frame(benef_H_indiv = numeric())
 for (x in 30:64) {
-  beneficios_por_edad_hombres <- (A*ax.n_ij(x,n = 110-x,i=0,1,5.8,inf=2.8818,"H")) +
-                           (B*ax.n_ij(x,n = 110-x,i=0,2,5.8,inf=2.8818,"H")) +
-                             (C*ax.n_ij(x,n = 110-x,i=0,3,5.8,inf=2.8818,"H")) +
-                               (D*ax.n_ij(x,n= 110-x,i=0,4,5.8,inf=2.8818,"H"))
-  beneficios_hombres <- rbind(beneficios_hombres, data.frame(Beneficios_hombres = beneficios_por_edad_hombres))
+  beneficios_por_edad_hombres <- (A*ax.n_ij(x,n = 110-x,i=0,j=1,sexo="H")) +
+                           (B*ax.n_ij(x,n = 110-x,i=0,j=2,sexo="H")) +
+                             (C*ax.n_ij(x,n = 110-x,i=0,j=3,sexo="H")) +
+                               (D*ax.n_ij(x,n= 110-x,i=0,j=4,sexo="H"))
+  benef_H_indiv <- rbind(benef_H_indiv, data.frame(benef_H_indiv = beneficios_por_edad_hombres))
 }
 
-edades_seleccionadas_hombres <- cbind(edades_seleccionadas_hombres, Beneficios_Hombres = beneficios_hombres)
-colnames(edades_seleccionadas_hombres)[5] <- "Beneficios_individuales_por_edad"
-beneficios_totales_por_edad <- edades_seleccionadas_hombres[, 5] * edades_seleccionadas_hombres[, 4]
-beneficios_totales_por_edad <- as.vector(beneficios_totales_por_edad)
-edades_seleccionadas_hombres <- cbind(edades_seleccionadas_hombres, beneficios_totales_por_edad)
+edades_selec_H <- cbind(edades_selec_H, benef_H_indiv = benef_H_indiv)
+colnames(edades_selec_H)[5] <- "Benef_ind_por_edad"
+benef_tot_por_edad <- edades_selec_H[, 5] * edades_selec_H[, 4]
+benef_tot_por_edad <- as.vector(benef_tot_por_edad)
+edades_selec_H <- cbind(edades_selec_H, benef_tot_por_edad)
 
-suma_beneficios_hombres <- sum(edades_seleccionadas_hombres$beneficios_totales_por_edad)
+suma_benef_H_indiv <- sum(edades_selec_H$benef_tot_por_edad)
+
 
 # Crear un vector vacío para almacenar los resultados de mujeres
-
-beneficios_mujeres <- data.frame(Beneficios_mujeres = numeric())
-
+benef_M_indiv <- data.frame(benef_M_indiv = numeric())
 for (x in 30:64) {
-  beneficios_por_edad_mujeres <- (A*ax.n_ij(x,n = 110-x,i=0,1,5.8,inf=2.8818,"M")) +
-    (B*ax.n_ij(x,n = 110-x,i=0,2,5.8,inf=2.8818,"M")) +
-    (C*ax.n_ij(x,n = 110-x,i=0,3,5.8,inf=2.8818,"M")) +
-    (D*ax.n_ij(x,n= 110-x,i=0,4,5.8,inf=2.8818,"M"))
-  beneficios_mujeres <- rbind(beneficios_mujeres, data.frame(Beneficios_mujeres = beneficios_por_edad_mujeres))
+  beneficios_por_edad_mujeres <- (A*ax.n_ij(x,n = 110-x,i=0,j=1,sexo="M")) +
+    (B*ax.n_ij(x,n = 110-x,i=0,j=2,sexo="M")) +
+    (C*ax.n_ij(x,n = 110-x,i=0,j=3,sexo="M")) +
+    (D*ax.n_ij(x,n= 110-x,i=0,j=4,sexo="M"))
+  benef_M_indiv <- rbind(benef_M_indiv, data.frame(benef_M_indiv = beneficios_por_edad_mujeres))
 }
 
-edades_seleccionadas_mujeres <- cbind(edades_seleccionadas_mujeres, Beneficios_Mujeres = beneficios_mujeres)
-colnames(edades_seleccionadas_mujeres)[5] <- "Beneficios_individuales_por_edad"
-beneficios_totales_por_edad_mujeres <- edades_seleccionadas_mujeres[, 5] * edades_seleccionadas_mujeres[, 4]
-beneficios_totales_por_edad_mujeres <- as.vector(beneficios_totales_por_edad_mujeres)
-edades_seleccionadas_mujeres <- cbind(edades_seleccionadas_mujeres, beneficios_totales_por_edad_mujeres)
+edades_selec_M <- cbind(edades_selec_M, benef_M_indiv = benef_M_indiv)
+colnames(edades_selec_M)[5] <- "Benef_ind_por_edad"
+benef_tot_por_edad_M <- edades_selec_M[, 5] * edades_selec_M[, 4]
+benef_tot_por_edad_M <- as.vector(benef_tot_por_edad_M)
+edades_selec_M <- cbind(edades_selec_M, benef_tot_por_edad_M)
 
-suma_beneficios_mujeres <- sum(edades_seleccionadas_mujeres$beneficios_totales_por_edad_mujeres)
+suma_benef_M_indiv <- sum(edades_selec_M$benef_tot_por_edad_M)
 
-# primas 
+##--- Primas -------------------------------------------------------------------
 
 # hombres 
 
 primas_hombres <- data.frame(Primas_hombres = numeric())
-
 for (x in 30:64) {
-  primas_por_edad_hombres <- (ax.n_ij(x,n = 110-x,i=0,0,5.8,inf=2.8818,"H")) +
-    (ax.n_ij(x,n = 110-x,i=0,1,5.8,inf=2.8818,"H")) +
-    (ax.n_ij(x,n = 110-x,i=0,2,5.8,inf=2.8818,"H")) 
+  primas_por_edad_hombres <- (ax.n_ij(x,n = 110-x,i=0,j=0,sexo="H")) +
+    (ax.n_ij(x,n = 110-x,i=0,j=1,sexo="H")) +
+    (ax.n_ij(x,n = 110-x,i=0,j=2,sexo="H")) 
   primas_hombres <- rbind(primas_hombres, data.frame(Primas_hombres = primas_por_edad_hombres))
 }
 
-edades_seleccionadas_hombres <- cbind(edades_seleccionadas_hombres, Primas_Hombres = primas_hombres)
-colnames(edades_seleccionadas_hombres)[7] <- "Primas_individuales_por_edad"
-primas_totales_por_edad <- edades_seleccionadas_hombres[, 7] * edades_seleccionadas_hombres[, 4]
-primas_totales_por_edad <- as.vector(primas_totales_por_edad)
-edades_seleccionadas_hombres <- cbind(edades_seleccionadas_hombres, primas_totales_por_edad)
+edades_selec_H <- cbind(edades_selec_H, Primas_Hombres = primas_hombres)
+colnames(edades_selec_H)[7] <- "primas_ind_por_edad "
+primas_tot_por_edad <- edades_selec_H[, 7] * edades_selec_H[, 4]
+primas_tot_por_edad <- as.vector(primas_tot_por_edad)
+edades_selec_H <- cbind(edades_selec_H, primas_tot_por_edad)
 
-primas_hombres_suma <- sum(edades_seleccionadas_hombres$primas_totales_por_edad)
+primas_hombres_suma <- sum(edades_selec_H$primas_tot_por_edad)
 
 # mujeres
 primas_mujeres <- data.frame(Primas_mujeres = numeric())
-
 for (x in 30:64) {
   primas_por_edad_mujeres <- (ax.n_ij(x,n = 110-x,i=0,0,5.8,inf=2.8818,"M")) +
     (ax.n_ij(x,n = 110-x,i=0,1,5.8,inf=2.8818,"M")) +
@@ -216,18 +212,18 @@ for (x in 30:64) {
   primas_mujeres <- rbind(primas_mujeres, data.frame(Primas_mujeres = primas_por_edad_mujeres))
 }
 
-edades_seleccionadas_mujeres <- cbind(edades_seleccionadas_mujeres, Primas_Mujeres = primas_mujeres)
-colnames(edades_seleccionadas_mujeres)[7] <- "Primas_individuales_por_edad"
-primas_totales_por_edad_mujeres <- edades_seleccionadas_mujeres[, 7] * edades_seleccionadas_mujeres[, 4]
-primas_totales_por_edad_mujeres <- as.vector(primas_totales_por_edad_mujeres)
-edades_seleccionadas_mujeres <- cbind(edades_seleccionadas_mujeres, primas_totales_por_edad_mujeres)
+edades_selec_M <- cbind(edades_selec_M, Primas_Mujeres = primas_mujeres)
+colnames(edades_selec_M)[7] <- "primas_ind_por_edad "
+primas_tot_por_edad_M <- edades_selec_M[, 7] * edades_selec_M[, 4]
+primas_tot_por_edad_M <- as.vector(primas_tot_por_edad_M)
+edades_selec_M <- cbind(edades_selec_M, primas_tot_por_edad_M)
 
-primas_mujeres_suma <- sum(edades_seleccionadas_mujeres$primas_totales_por_edad_mujeres)
+primas_mujeres_suma <- sum(edades_selec_M$primas_tot_por_edad_M)
 
 
 # principio de equivalencia
 
-beneficios_totales <- suma_beneficios_mujeres + suma_beneficios_hombres
+beneficios_totales <- suma_benef_M_indiv + suma_benef_H_indiv
 
 primas_0.95 <- (primas_hombres_suma + primas_mujeres_suma)*0.95
 
@@ -236,3 +232,4 @@ costo_incial <- 0.15
 total_primas <- costo_incial + primas_0.95
 
 prima <- beneficios_totales / total_primas
+
