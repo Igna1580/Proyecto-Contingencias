d9a21b877bc13ee3f4978700edee43227150642e
montserratbeirute <143130080+montserratbeirute@users.noreply.github.com>
2023-11-09 14:07:08 -0600

    pull

--- Tabla de vida.R
+++ Tabla de vida.R
@@ -42,8 +42,8 @@ for(fila in 1:nrow(Prob_Trans_Hombres)){
 #--- Poblacion -----------------------------------------------------------------
 edades <- 31:65
 
-porcentajes <- c(0.05, 0.05, 0.08, 0.08, 0.10, 0.10, 0.15, 0.15, 0.20, 0.20, 0.25, 0.25, 0.30, 0.30, 0.35,
-                 rep(0.6, 20))  # Luego, 20 porcentajes uniformes de 0.6
+porcentajes <- c(0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.15, 0.15,
+                 rep(0.13, 20))  # Luego, 20 porcentajes uniformes de 0.6
 # Caso hombres 
 
 # Crear un dataframe para las edades
